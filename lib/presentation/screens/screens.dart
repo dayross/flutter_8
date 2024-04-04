a8faa17ca5651c4f7e91a1a03b10d3487f39dc9d
Diana Zapata <dianarocio.czapata@gmail.com>
2024-04-04 16:41:25 -0600

    Creado de distintas screens (vacias) y actualizado de "barril"

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:flutter_8/presentation/screens/animated/animated_screen.dart';
+export 'package:flutter_8/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:flutter_8/presentation/screens/inifinite_scroll/infinite_scroll_screen.dart';
+export 'package:flutter_8/presentation/screens/progress/progress_screen.dart';
+export 'package:flutter_8/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:flutter_8/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:flutter_8/presentation/screens/buttons/buttons_screens.dart';
 export 'package:flutter_8/presentation/screens/cards/cards_screen.dart';
 export 'package:flutter_8/presentation/screens/home/home_screen.dart';
