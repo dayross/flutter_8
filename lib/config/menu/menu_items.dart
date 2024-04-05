970341e4c027e8086057788f466f1b5d75d36ad4
Diana Zapata <dianarocio.czapata@gmail.com>
2024-04-05 11:53:02 -0600

    Añadido de pantalla de distintos snackbars y popUps

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -25,8 +25,13 @@ const appMenuItems = <MenuItem>[
       link: '/cards',
       icon: Icons.credit_card),
   MenuItem(
-      title: 'ProgressIndicators',
+      title: 'Progress Indicators',
       subtitle: 'Generales y controlados',
       link: '/progress',
       icon: Icons.refresh_rounded),
+  MenuItem(
+      title: 'Snackbars y dialogos',
+      subtitle: 'Pequeños popUps en diversas areas de la pantalla',
+      link: '/snackbars',
+      icon: Icons.info_outline),
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackBarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -6,6 +7,68 @@ class SnackBarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y dialogos'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(
+                      context: context, children: [Text('Easter Egg\nHi')]);
+                },
+                child: Text('Licencias potaxianas')),
+            FilledButton.tonal(
+                onPressed: () {
+                  openDialog(context);
+                },
+                child: Text('Mostrar dialogo de pantalle')),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackBar(context),
+        label: const Text('Mostrar SnackBar'),
+        icon: Icon(Icons.gamepad_rounded),
+      ),
+    );
+  }
+
+  void openDialog(BuildContext context) {
+    // builder significa que es algo que se va a construir en tiempo de ejecución
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+              title: Text('¿Estas seguro?'),
+              content: Icon(
+                Icons.emoji_nature,
+                size: 80,
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      context.pop();
+                    },
+                    child: Text('Cancelar')),
+                FilledButton(onPressed: () {}, child: Text('Aceptar'))
+              ],
+            ));
+  }
+
+  void showCustomSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final snakcbar = SnackBar(
+      content: const Text('Hola Mundo'),
+      duration: Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'Hola!',
+        onPressed: () {},
+      ),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snakcbar);
   }
 }
